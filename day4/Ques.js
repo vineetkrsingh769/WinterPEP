97f987b687bf4087ddc8b2b3254666a824e2e8d7
Vineet Kumar Singh <118097275+vineetkrsingh769@users.noreply.github.com>
2025-01-19 22:18:33 +0530

    Add files via upload

---
+++ day4/Ques.js
@@ -0,0 +1,61 @@
+// // 1. Replace the function expressions with arrow functions in the following code:
+
+// function ask(question, yes, no) {  
+//     if (confirm(question)) yes();  
+//     else no();  
+// }  
+// ask(  
+//     "Do you agree?",  
+//     function() { alert("You agreed."); },  
+//     function() { alert("You canceled the execution."); }  
+// ); 
+
+
+
+
+
+// // 2. Identify the output of the following code:
+
+// const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];  
+// console.log(materials.map((material) => material.length));
+
+
+
+
+// // 3. Write an arrow function called square that takes a number as an argument and 
+// returns its square.
+
+
+
+
+
+// // 4. Given an array of numbers, use an arrow function to create a new array 
+// containing the squares of those numbers. For example, if the input is [1, 2, 3], 
+// the output should be [1, 4, 9].
+
+
+
+
+
+
+// // 5. Use an arrow function to reverse a given string. For example, if the input is 
+// "hello", the output should be "olleh".
+
+
+
+
+
+
+
+
+// // 6. Create a chainable object using arrow functions that allows setting a value and 
+// then adding or multiplying it by a number. For example:
+
+// const result = chainable(5).add(3).multiply(2);  
+// // result should be 16
+
+
+
+
+
+
---
+++ day4/arrow.js
@@ -0,0 +1,19 @@
+// const test=(x,y,z)=>{
+//     console.log(x+y+z)
+// }
+// test(5,6,7)
+
+
+// const person = (firstName, lastName) => {
+//     return {
+//         first: firstName,
+//         last: lastName
+    
+//     };
+// };
+
+// console.log(person("Panjab", "LPU"))
+
+
+let sayhi=()=>alert("hello");
+sayhi();
